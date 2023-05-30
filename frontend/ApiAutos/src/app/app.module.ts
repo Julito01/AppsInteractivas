462475fe9098e544eeaf0fd6b2f4823e960ebe43
Julio Dechert <julitod55@hotmail.com>
2023-05-30 18:21:38 -0300

    Initial commit

---
+++ frontend/ApiAutos/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
